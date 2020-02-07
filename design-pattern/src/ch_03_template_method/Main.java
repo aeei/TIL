229b4523c51152e4329e36624ddb6e4d60814cd4
Kelly <0xae80@gmail.com>
2020-02-07 09:23:20 +0900

    Initial commit

---
+++ design-pattern/src/ch_03_template_method/Main.java
@@ -0,0 +1,7 @@
+package ch03_template_method;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ design-pattern/src/ch_03_template_method/StringDisplay.java
@@ -0,0 +1,4 @@
+package ch_03_template_method;
+
+public class StringDisplay {
+}
