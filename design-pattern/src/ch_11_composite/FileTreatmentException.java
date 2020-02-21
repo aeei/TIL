0523c25a0f3e8c75b5ef43e7596f05ba12f2f903
Kelly <0xae80@gmail.com>
2020-02-21 11:04:09 +0900

    Add Chapter.11 Composite

---
+++ design-pattern/src/ch_11_composite/FileTreatmentException.java
@@ -0,0 +1,13 @@
+package ch_11_composite;
+
+public class FileTreatmentException extends RuntimeException {
+
+  public FileTreatmentException() {
+
+  }
+
+  public FileTreatmentException(String msg) {
+    super(msg);
+  }
+
+}
