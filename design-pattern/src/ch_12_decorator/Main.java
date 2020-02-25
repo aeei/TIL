5bba5d523c28a5e0713e9d408fa732692a72360f
Kelly <0xae80@gmail.com>
2020-02-25 11:20:19 +0900

    Add Cnapter.12 Decorator

---
+++ design-pattern/src/ch_12_decorator/Main.java
@@ -0,0 +1,30 @@
+package ch_12_decorator;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Display b1 = new StringDisplay("Hello, world.");
+    Display b2 = new SideBorder(b1, '#');
+    Display b3 = new FullBorder(b2);
+
+    b1.show();
+    b2.show();
+    b3.show();
+
+    Display b4 = new SideBorder(
+        new FullBorder(
+            new FullBorder(
+                new SideBorder(
+                    new FullBorder(
+                        new StringDisplay("You do have a plan!")
+                    ),
+                    '*'
+                )
+            )
+        ),
+        '/'
+    );
+
+    b4.show();
+  }
+}
